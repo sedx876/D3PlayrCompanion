7641abd79f0890e48b05363329c154a9954f4df7
sedx876 <swatkins876@gmail.com>
2020-09-13 11:45:25 -0400

    namespaced routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :characters
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :characters
+    end
+  end
 end
