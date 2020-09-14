bc106f0458e007c0f5cb03e269057fde67f7f5d0
sedx876 <swatkins876@gmail.com>
2020-09-13 20:37:34 -0400

    intial store in store.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import store from './store.js'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={ store }>
+    <Router><App /></Router>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ frontend/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import currentUser from './reducers/currentUser'
+import thunk from 'redux-thunk'
+
+const reducer = combineReducers({
+  currentUser,
+  
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+export default store
