b585e435f74588ddfa3ac0a638ae5906a2317baf
sedx876 <swatkins876@gmail.com>
2020-09-13 20:43:10 -0400

    currentUser action

---
+++ frontend/src/actions/currentUser.js
@@ -0,0 +1,37 @@
+
+
+export const setCurrentUser = user => {
+  return {
+    type: "SET_CURRENT_USER",
+    user
+  }
+}
+
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}
+
+export const getCurrentUser = () => {
+  return dispatch => {
+    return fetch("http://localhost:3001/api/v1/get_current_user", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          console.log("here")
+          dispatch(setCurrentUser(response.data))
+          //dispatch(getMyTrips())
+        }
+      })
+      .catch(console.log)
+  }
+}
