a522dba5267ef448be00e5fa6d83049f6f705490
sedx876 <swatkins876@gmail.com>
2020-09-16 07:08:37 -0400

    myCharacters.js action

---
+++ frontend/src/actions/myCharacters.js
@@ -0,0 +1,31 @@
+
+
+
+
+export const setMyCharacters = characters => {
+  return {
+    types: "SET_MY_CHARACTERS",
+    characters
+  }
+}
+
+export const getMyCharacters = () => {
+  return dispatch => {
+    return fetch('http://localhost:3001/api/v1/characters', {
+      credentials: 'include',
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(r => r.json())
+    .then(response => {
+      if (response.error) {
+        alert(response.error)
+      } else {
+        dispatch(setMyCharacters(response.data))
+      }
+    })
+    .catch(console.log)
+  }
+}
