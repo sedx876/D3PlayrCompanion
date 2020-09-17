a4cf9ab3efdf043d5dead28fa8cd3bdf7a5a96f0
sedx876 <swatkins876@gmail.com>
2020-09-17 09:51:17 -0400

    update myCharacter reducer

--- frontend/src/reducers/myCharacters.js
+++ frontend/src/reducers/myCharacters.js
@@ -4,6 +4,12 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_MY_CHARACTERS":
       return action.characters
+    case "ADD_CHARACTERS":
+      return state.concat(action.character)
+    case "UPDATE_CHARACTER":
+      return state.map(character => character.id === action.characterId ? false : true)
+    case "CLEAR_CHARACTERS":
+      return initialState
     default:
       return state
   }
