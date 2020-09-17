140b2a64128a7bd516ab8eb73cff37b7c050644c
sedx876 <swatkins876@gmail.com>
2020-09-17 10:31:21 -0400

    characterform component fixes

--- frontend/src/components/CharacterForm.js
+++ frontend/src/components/CharacterForm.js
@@ -1,39 +1,78 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { updateCharacterForm } from '../actions/characterForm'
 
-const CharacterForm = ({ formData, userId }) => {
+const CharacterForm = ({ formData, updateCharacterForm, handleSubmit, userId, character, editMode }) => {
 
   const { title, characterKlass, helm, shoulders, gloves, 
           chestArmor, belt, pants, boots, bracers, amulet, 
           ring1, ring2, weapon, offhand, gemNotes, kanaisCube, 
           activeSkills, passiveSkills, generalBuildNotes } = formData
 
-  return (
+  const handleChange = event => {
+    console.log('triggered handleChange')
+    const { name, value } = event.target
+    updateCharacterForm(name, value)
+  }
 
-    <form>
-      <input placeholder='Title of Your Build' name='title' value={title} isRequired />
-      <input placeholder='Character Class EX: Demon Hunter..Necromancer...' name='characterKlass' value={characterKlass} />
-      <input placeholder='Helm' name='helm' value={helm} />
-      <input placeholder='Shoulders' name='shoulders' value={shoulders} />
-      <input placeholder='Gloves' name='gloves' value={gloves} />
-      <input placeholder='Chest Armor' name='chestArmor' value={chestArmor} />
-      <input placeholder='Belt' name='belt' value={belt} />
-      <input placeholder='Pants' name='pants' value={pants} />
-      <input placeholder='Boots' name='boots' value={boots} />
-      <input placeholder='Bracers' name='bracers' value={bracers} />
-      <input placeholder='Amulet' name='amulet' value={amulet} />
-      <input placeholder='Ring 1' name='ring1' value={ring1} />
-      <input placeholder='Ring 2' name='ring2' value={ring2} />
-      <input placeholder='Weapon' name='weapon' value={weapon} />
-      <input placeholder='Offhand' name='offhand' value={offhand} />
-      <textarea placeholder='Gems EX: Legendaries and Common Gems to Socket' name='gemNotes' value={gemNotes} />
-      <textarea placeholder="Kanai's Cube What Legendary Powers to Extract" name='kanaisCube' value={kanaisCube} />
-      <textarea placeholder='Active Skills' name='activeSkills' value={activeSkills} />
-      <textarea placeholder='Passive Skills' name='passiveSkills' value={passiveSkills} />
-      <textarea placeholder='Random Build Notes' name='generalBuildNotes' value={generalBuildNotes} />
-      <input type='submit' value={'Create Character'} />
+  return (
+  <div className="card text-white bg-secondary">
+    <form onSubmit={event => {
+      event.preventDefault()
+      handleSubmit(formData)
+    }}>
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Title of Your Build' name='title' value={title} isRequired />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Character Class EX: Demon Hunter..Necromancer...' name='characterKlass' value={characterKlass} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Helm' name='helm' value={helm} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Shoulders' name='shoulders' value={shoulders} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Gloves' name='gloves' value={gloves} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Chest Armor' name='chestArmor' value={chestArmor} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Belt' name='belt' value={belt} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Pants' name='pants' value={pants} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Boots' name='boots' value={boots} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Bracers' name='bracers' value={bracers} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Amulet' name='amulet' value={amulet} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Ring 1' name='ring1' value={ring1} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Ring 2' name='ring2' value={ring2} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Weapon' name='weapon' value={weapon} />
+      <br />
+      <input className="form-control col-sm-10" onChange={handleChange} placeholder='Offhand' name='offhand' value={offhand} />
+      <br />
+      <textarea className="form-control col-sm-10" onChange={handleChange} placeholder='Gems EX: Legendaries and Common Gems to Socket' name='gemNotes' value={gemNotes} />
+      <br />
+      <textarea className="form-control col-sm-10" onChange={handleChange} placeholder="Kanai's Cube What Legendary Powers to Extract" name='kanaisCube' value={kanaisCube} />
+      <br />
+      <textarea className="form-control col-sm-10" onChange={handleChange} placeholder='Active Skills' name='activeSkills' value={activeSkills} />
+      <br />
+      <textarea className="form-control col-sm-10" onChange={handleChange} placeholder='Passive Skills' name='passiveSkills' value={passiveSkills} />
+      <br />
+      <textarea className="form-control col-sm-10" onChange={handleChange} placeholder='Random Build Notes' name='generalBuildNotes' value={generalBuildNotes} />
+      <br />
+      <input className="btn btn-secondary" type='submit' value={editMode ? 'Update Trip' : 'Create Character'} />
     </form>
+    </div>
   )
 }
 
-export default CharacterForm
+const mapStateToProps = state => {
+  const userId = state.currentUser ? state.currentUser.id : ''
+  return {
+    formData: state.characterForm,
+    userId
+  }
+}
+
+export default connect(mapStateToProps, { updateCharacterForm }) (CharacterForm)
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,12 +4,14 @@ import thunk from 'redux-thunk'
 import loginForm from './reducers/loginForm'
 import signupForm from './reducers/signupForm'
 import myCharacters from './reducers/myCharacters'
+import characterForm from './reducers/characterForm'
 
 const reducer = combineReducers({
   currentUser,
   loginForm,
   signupForm,
   myCharacters,
+  characterForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
