6b017d755cce311b16b6f2f95b54e007e5d005e2
sedx876 <swatkins876@gmail.com>
2020-09-18 07:03:41 -0400

    added petchecklist controller

--- backend/app/controllers/api/v1/petchecklists_controller.rb
+++ backend/app/controllers/api/v1/petchecklists_controller.rb
@@ -1,2 +1,18 @@
-class PetchecklistsController < ApplicationController
+class Api::V1::PetchecklistsController < ApplicationController
+  before_action :set_petchecklist, only: [:show, :update, :destroy]
+
+  def index
+    @petchecklists = Petchecklist.all
+    render json: @petchecklists
+  end
+
+  private 
+
+  def set_petchecklist
+    @petchecklist = Petchecklist.find(params[:id])
+  end
+
+  def petchecklist_params
+    params.require(:petchecklist).permit(:name, :done)
+  end
 end
