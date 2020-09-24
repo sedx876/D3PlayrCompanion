0b3958dc2e428fc34d1fb30233548c38017e7017
sedx876 <swatkins876@gmail.com>
2020-09-24 06:30:04 -0400

    added item associations and API::V1 namespacing to controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :items
   post "/api/v1/login", to: "api/v1/sessions#create"
   post "/api/v1/signup", to: "api/v1/users#create"
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
@@ -9,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :characters
-        end
+      resources :items
+    end
   end
 end
