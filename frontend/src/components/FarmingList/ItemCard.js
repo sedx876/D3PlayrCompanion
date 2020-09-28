3d17ec3e2b79dd9599d36cbe8bdd9228449fbd7f
sedx876 <swatkins876@gmail.com>
2020-09-28 09:13:05 -0400

    added github svg to footer

--- frontend/src/components/FarmingList/ItemCard.js
+++ frontend/src/components/FarmingList/ItemCard.js
@@ -4,12 +4,16 @@ import '../../styles/farmingList.css'
 
 const ItemCard = ({ item }) => {
   return (
+
+item ?
+
     <div className="card border-info mb-3">
       <div className="card-header"><h3><span>{item.attributes.name}</span></h3>
       <p className="card-text"><span>Notes:</span> {item.attributes.notes}</p>
       <Link className="btn btn-outline-warning" to={`/items/${item.id}/edit`}>Edit or Delete this Character</Link>
       </div>
     </div>
+    : <p>Farming Item not found</p>
   )
 }
 
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../styles/footer.css'
+import logo from  '../images/github-alt-brands.svg'
 
 
 const Footer = () => {
@@ -8,6 +9,7 @@ const Footer = () => {
     <footer className="footer text-white">
       <p>&copy;2020   <span role="img" aria-label="devil">👹</span>D3PlayrCompanion<span role="img" aria-label="devil">👹</span> --Sharon Watkins React/Redux Project For Flatiron School.</p>
       <p>This app has no affiliation with Diablo 3 or Blizzard Entertainment, INC.</p>
+      <a href="https://github.com/sedx876/D3PlayrCompanion" target="_blank" rel="noopener noreferrer"><img className='gH' src={logo}/></a>
     </footer>
   )
 }
