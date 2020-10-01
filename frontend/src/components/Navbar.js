e3a275dadd50d36bf9f48a592ff1aa6cf6324016
sedx876 <swatkins876@gmail.com>
2020-10-01 15:44:30 -0400

    Added images to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,6 +3,13 @@ import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import Logout from './Users/Logout'
 import '../styles/Navbar.css'
+import theD3 from '../images/theD3.png'
+import portal from '../images/portal.png'
+import dLogo from '../images/dLogo.png'
+import tyr from '../images/tyr.png'
+import barLogo from '../images/barLogo.png'
+import dhLogo from '../images/dhLogo.png'
+import monkLogo from '../images/monkLogo.png'
 
 
 
@@ -11,7 +18,7 @@ const Navbar = ({ currentUser, loggedIn }) => {
   return(
 
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <NavLink className="nav-link" exact activeClassName="active" to='/home'>D3PlayrCompanion</NavLink>
+    <NavLink className="nav-link" exact activeClassName="active" to='/home'><img className='theD3' alt="D3 Logo" src={theD3}/>D3PlayrCompanion</NavLink>
 
     <button className="navbar-toggler" 
             type="button" 
@@ -26,22 +33,22 @@ const Navbar = ({ currentUser, loggedIn }) => {
   <div className="collapse navbar-collapse" id="navbarColor01">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item">
-        <NavLink className="nav-link" exact activeClassName="active" to='/about'>About</NavLink>
+        <NavLink className="nav-link" exact activeClassName="active" to='/about'><img className='dLogo' alt="D3 Logo" src={dLogo}/>About</NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" exact activeClassName="active" to='/characters'>Characters</NavLink>
+        <NavLink className="nav-link" exact activeClassName="active" to='/characters'><img className='tyr' alt="tyr logo" src={tyr}/>Characters</NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" exact activeClassName="active" to='/characters/new'>Add New Character Build</NavLink>
+        <NavLink className="nav-link" exact activeClassName="active" to='/characters/new'><img className='barLogo' alt="barbarian logo" src={barLogo}/>Add New Character Build</NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" exact activeClassName="active" to='/farminglist'>Farming List</NavLink>
+        <NavLink className="nav-link" exact activeClassName="active" to='/farminglist'><img className='dhLogo' alt="dh logo" src={dhLogo}/>Farming List</NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" exact activeClassName="active" to='/farminglist/new'>Add Item to Farming List</NavLink>
+        <NavLink className="nav-link" exact activeClassName="active" to='/farminglist/new'><img className='monkLogo' alt="monk logo" src={monkLogo}/>Add Item to Farming List</NavLink>
       </li>
       <li className="nav-item">
-        <NavLink className="nav-link" exact activeClassName="active" to='/links'>More Diablo</NavLink>
+        <NavLink className="nav-link" exact activeClassName="active" to='/links'><img className='portal' alt="portal" src={portal}/>Diablo Links</NavLink>
       </li>
     </ul>
     { loggedIn ? <><p id="loggedin">Logged in as {currentUser.attributes.username}</p><Logout /></> : null}
